19afae93944fd772053688978a567ed6ea95503b
yashjain12yj <yashjain12yj@gmail.com>
2018-03-16 23:23:00 +0530

    minor changes

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -9,7 +9,7 @@ const databaseUrl = require('./credentials').DBURL;
  */
 const serverConfigs = {
   PRODUCTION: process.env.NODE_ENV === 'production',
-  PORT: 5000,
+  PORT: process.env.PORT || 8080,
   ROOT: path.resolve(__dirname, '..'),
   DBURL: databaseUrl,
 };
