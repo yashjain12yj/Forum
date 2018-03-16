e9a53be73bfecb701774620b6841d14c01f3fc66
yashjain12yj <yashjain12yj@gmail.com>
2018-03-16 23:08:11 +0530

    minor changes

--- config/credentials.js
+++ config/credentials.js
@@ -1,6 +1,6 @@
 module.exports = {
   GITHUB_CLIENT_ID: 'f2d298181cdd9ed9f0df',
   GITHUB_CLIENT_SECRET: '372e60f2e65b4f18d154b3f5bc6927bdb877450d',
-  GITHUB_CALLBACK_URL: 'localhost:8080/api/user/authViaGitHub/callback',
+  GITHUB_CALLBACK_URL: 'https://yjforum.herokuapp.com/api/user/authViaGitHub/callback',
   DBURL: 'mongodb://yashjain12yj:123456@ds223738.mlab.com:23738/reforum',
 };
